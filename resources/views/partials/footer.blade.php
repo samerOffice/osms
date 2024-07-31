71969dd6887037c5fafa39c728302fe4fbc72871
Fahad Ahmed <fahadahmedsam@gmail.com>
2024-07-31 11:45:13 +0600

    delete

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-  <strong>Copyright &copy; 2024 <a href="https://otitheesoftwaresolution.com/">Otithee Software Solution Limited</a>.</strong>
-  All rights reserved.
- 
-</footer>
