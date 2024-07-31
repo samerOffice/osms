ddc824fb86fc368b25f3392c6b09a88816d3431a
Fahad Ahmed <fahadahmedsam@gmail.com>
2024-07-31 13:00:00 +0600

    31New2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -88,7 +88,7 @@
 <!-- AdminLTE App -->
 <script src="{{asset('public/dist/js/adminlte.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('public/dist/js/demo.js')}}"></script>
+<!-- <script src="{{asset('public/dist/js/demo.js')}}"></script> -->
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{asset('public/dist/js/pages/dashboard.js')}}"></script>
 <script>
